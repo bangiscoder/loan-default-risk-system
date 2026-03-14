8abf09acf3435d9983a3baf53c5ac4c9385d6916
bangiscoder <bangiscoder@gmail.com>
2026-03-14 19:15:27 +0100

    Initial Project Setup

---
+++ src/load_dataset.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo
+import pandas as pd
+
+dataset = fetch_ucirepo(id=144)
+
+X = dataset.data.features
+y = dataset.data.targets
+
+data = pd.concat([X, y], axis=1)
+
+data.to_csv("data/german_credit_data.csv", index=False)
+
+print("Dataset saved successfully!")
+print(data.head())
